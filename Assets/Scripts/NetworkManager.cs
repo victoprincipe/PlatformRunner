a39ed030ceb095ad998090d442dffeae80020228
Runewather <victo_10_@hotmail.com>
2019-05-20 17:06:32 -0300

    Json parser

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+
+public class NetworkManager : MonoBehaviour
+{
+
+    [SerializeField]
+    int udpPort = 9082;
+    [SerializeField]
+    string host = "127.0.0.1";
+    UTF8Encoding encoding = new UTF8Encoding();
+    WaitForSeconds UdpDelay = new WaitForSeconds(0.3f);
+    IPEndPoint UdpEndPoint = null;
+    UdpClient client = null;
+
+    [System.Serializable]
+    public class JsonObject : System.Object
+    {
+        public string[] values;
+
+        public JsonObject(string[] array)
+        {
+            values = array;
+        }
+    }
+
+    public void UdpConnect()
+    {
+        client = new UdpClient();
+    }
+
+    public void UdpDisconnect()
+    {
+        client.Close();
+    }
+
+    void ParseDataToJson(byte[] data)
+    {
+        string JsonString = encoding.GetString(data);
+        JsonObject Json = JsonUtility.FromJson<JsonObject>(JsonString);
+    }
+
+    IEnumerator ReceiveData()
+    {
+        byte[] data = null;
+        while (true)
+        {
+            if (client.Available > 0)
+            {
+                yield return null;
+                data = client.Receive(ref UdpEndPoint);
+                ParseDataToJson(data);
+                data = null;
+            }
+            yield return UdpDelay;
+        }
+    }
+
+    public void SendCommand(params string[] values)
+    {
+        JsonObject Json = new JsonObject(values);
+        string Jsonstring = JsonUtility.ToJson(Json);
+        byte[] data = encoding.GetBytes(Jsonstring);
+        client.Send(data, data.Length, host, udpPort);
+    }
+
+    void Start()
+    {        
+        UdpConnect();
+        StartCoroutine(ReceiveData());
+    }
+}
--- GameServerNode/index.js
+++ GameServerNode/index.js
@@ -1,31 +1,31 @@
-var udpPort = 9082;
-var url = require ('url');
 var udp = require ('dgram');
-var http = require ('http');
-var request = require ('request');
-var udpServer = udp.createSocket ('udp4').bind (9082, '127.0.0.1');
+
+const host = '127.0.0.1'
+const udpPort = 3030
+
+var udpServer = udp.createSocket ('udp4').bind (udpPort, host)
 
 udpServer.on ('close', function (err) {
-    console.log ("UDP close");
-});
+    console.log ("UDP close")
+})
 
 udpServer.on ('error', function (err) {
-    console.log ("UDP Error: " + err.toString ());
-});
+    console.log ("UDP Error: " + err.toString ())
+})
 
 udpServer.on ('message', function (data, remote) {
-    console.log ("UDP received");
-    console.log("DATA: " + data);
-    console.log("IP: " + remote.address);
+    console.log ("UDP received")
+    console.log("DATA: " + data)
+    console.log("IP: " + remote.address)
     let msg = {
         x: 50,
         y: 30
-    };
-    msg = JSON.stringify(msg);
-    udpServer.send( msg, 0, msg.length, remote.port, remote.address );
+    }
+    msg = JSON.stringify(msg)
+    udpServer.send( msg, 0, msg.length, remote.port, remote.address )
 });
 
 udpServer.on ('listening', function () {
-    var address = udpServer.address ();
-    console.log ("UDP Listening On IP: " + address.address + " at Port: " + address.port);
+    var address = udpServer.address ()
+    console.log ("UDP Listening On IP: " + address.address + " at Port: " + address.port)
 });
